bfc81419bd663fdb7533a1b6cf32367e4f252d46
cadmin <agj030127@gmail.com>
2025-04-30 10:53:58 +0900

    9주차 깃허브 테스트트

--- js/js_popup.js
+++ js/js_popup.js
@@ -1,5 +1,5 @@
 // var close_time; // 시간 정보
-// var close_time2 = 10; // 10초 설정
+// var close_time2 = 50; // 50초 설정
 // clearTimeout(close_time); // 재호출 정지
 // close_time= setTimeout("close_window()", 10000);
 // // 1/1000 초 지정, 바로 시작
@@ -15,8 +15,41 @@
 // }
 
 function pop_up() {
-    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10"); //   ../는 상위폴더를 의미함. 위치를 잘 설정해야함.
+    var cookieCheck = getCookie("popupYN");
+    if (cookieCheck != "N"){
+    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
     }
+    
+}
+function closePopup() {
+    if (document.getElementById('check_popup').value) {
+    setCookie("popupYN", "N", 1);
+    console.log("쿠키를 설정합니다.");
+    self.close();
+    }
+}
+    
+
+function setCookie(name, value, expiredays) {
+    var date = new Date();
+    date.setDate(date.getDate() + expiredays);
+    document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "; path=/";
+    }
+    function getCookie(name) {
+        var cookie = document.cookie;
+     console.log("쿠키를 요청합니다.");
+             if (cookie != "") {
+                 var cookie_array = cookie.split("; ");
+             for ( var index in cookie_array) {
+                 var cookie_name = cookie_array[index].split("=");
+             if (cookie_name[0] == "popupYN") {
+         return cookie_name[1];
+                                 }
+            }
+        }
+        return ;
+    }
+
     function over(obj) {
         obj.src="image/LOGO.svg";
         }
